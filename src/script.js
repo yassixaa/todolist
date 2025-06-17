446fcaf54464c4821fdcad228b669d593ea3152e
Yassica Umaparan <yassica.umaparan@students.unibe.ch>
2025-06-17 13:50:16 +0200

    renamed docs and wip css

--- src/script.js
+++ src/script.js
@@ -46,7 +46,8 @@ function newElement() {
   span.appendChild(txt);
   li.appendChild(span);
 
-  for (i = 0; i < close.length; i++) {
+  var close = document.getElementsByClassName("close");
+  for (let i = 0; i < close.length; i++) {
     close[i].onclick = function() {
       var div = this.parentElement;
       div.style.display = "none";
